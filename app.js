06ca3680c754cafa42b74bce3dc364c1daf20989
Abdoul Aziz SY <azizsy88@gmail.com>
2019-12-24 10:33:52 +0100

    adding node project

---
+++ app.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+const express = require('express');
+const bodyparser = require('body-parser');
+const xmlparser = require('express-xml-bodyparser');
+
+
+var app = express();
+app.use(xmlparser());
+app.use(bodyparser.json());
+
+var mysqlConnection=mysql.createConnection({
+  host:"localhost",
+  user:"root",
+  password:"root",
+  database:"magasin",
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+});
+
+mysqlConnection.connect((err) => {
+    if(!err)
+        console.log('Db connection succeded');
+    else
+        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+});
